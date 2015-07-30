441b53e92ca4479f61d8e18ebb647932e43c8882
izy521 <kyekye321@gmail.com>
2015-07-30 17:55:34 +0300

    Initial Commit
    Discord bots!

---
+++ example.js
@@ -0,0 +1,34 @@
+var Discordbot = require('node-discord');
+var bot = new Discordbot({
+	token: "", //Your bot's token
+	chats: [""] //Chat room as the first string in an array. An array for future-proofing.
+});
+
+bot.on("ready", function() {
+	console.log("Connected!");
+	console.log("Logged in as: ");
+	console.log(bot.username);
+	console.log(bot.id);
+	console.log("----------");
+});
+
+bot.on("message", function(user, userID, chatID, message, extras) {
+	console.log(user + " - " + userID);
+	console.log("in " + chatID);
+	console.log(message);
+	console.log("----------");
+	
+	if (message == "Hello, " + bot.username) {
+		bot.sendMessage("Hello, " + user); //Sending a string message.
+	}
+	if (message == "echo") {
+		bot.sendMessage({ //Sending an object message.
+			target: chatID,
+			message: message
+		});
+	}
+});
+
+bot.on("error", function(message) {
+	console.log(message);
+});
