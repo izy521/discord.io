7b32209493547945146d813924ce86ba3e4b5336
izy521 <kyekye321@gmail.com>
2016-03-25 06:19:29 +0300

    Updated ffmpeg example
    Minor change, but no 'callback hell'

--- examples/ffmpeg.js
+++ examples/ffmpeg.js
@@ -13,22 +13,24 @@ bot.on('ready', function() {
     console.log(bot.username + " - (" + bot.id + ")");
 	
 	bot.joinVoiceChannel(voiceChannelID, function() {
-		bot.getAudioContext({channel: voiceChannelID, stereo: true}, function(stream) {
-			var ffmpeg = spawn('ffmpeg' , [ //Or 'avconv', if you have it instead
-				'-i', file,
-				'-f', 's16le',
-				'-ar', '48000',
-				'-ac', '2',
-				'pipe:1'
-			], {stdio: ['pipe', 'pipe', 'ignore']});
-			
-			ffmpeg.stdout.once('readable', function() {
-				stream.send(ffmpeg.stdout);
-			});
-			
-			ffmpeg.stdout.once('end', function() {
-				//The file's done
-			});
-		});
+		bot.getAudioContext({channel: voiceChannelID, stereo: true}, handleStream);
 	});
 });
+
+function handleStream(stream) {
+	var ffmpeg = spawn('ffmpeg' , [ //Or 'avconv', if you have it instead
+		'-i', file,
+		'-f', 's16le',
+		'-ar', '48000',
+		'-ac', '2', //If you want one audio channel (mono), you can omit `stereo: true` in `getAudioContext`
+		'pipe:1'
+	], {stdio: ['pipe', 'pipe', 'ignore']});
+			
+	ffmpeg.stdout.once('readable', function() {
+		stream.send(ffmpeg.stdout);
+	});
+			
+	ffmpeg.stdout.once('end', function() {
+		//The file's done
+	});
+}
