e44cf9f8a0f57aa453fbc04fe76c99cf0e3c42ec
Arthur <s4n1k@users.noreply.github.com>
2016-12-23 02:49:19 -0200

    fixes

--- examples/auto-role.js
+++ examples/auto-role.js
@@ -1,19 +1,19 @@
 var Discord = require('discord.io');
+var drole = ""; /* roleid to be applied when someone joins the server */
+var serverid = ""; /* your server id */
 var bot = new Discord.Client({
 	autorun: true, /* If false, you need to connect to the server using bot.connect(); */
 	token: "" /* your discordapp token */
 });
 
-var drole = ""; /* roleid to be applied when someone joins the server */
-var serverid = ""; /* your server id */
-
 bot.on('ready', function() {
-console.log("Successfully connected: " + bot.username + " - (" + bot.id + ")");
+	console.log("Successfully connected: " + bot.username + " - (" + bot.id + ")");
 });
 
 bot.on('guildMemberAdd', function(callback) { /* Event called when someone joins the server */
-  bot.addToRole({"serverID":serverid,"userID":callback["id"],"roleID":drole},function(err,response) {
-   if (!err) return; /* Failed to apply role */ 
+if(callback.guild_id == serverid)
+  bot.addToRole({"serverID":serverid,"userID":callback.id,"roleID":drole},function(err,response) {
+   if (err) console.error(err); /* Failed to apply role */
     /* some code */
   });
  });
